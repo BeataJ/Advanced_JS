bb08abbcebab59086c46d0506522ef04b2d395f3
beataj <beata@jasinski.com>
2023-06-04 16:24:55 -0700

    create objects

---
+++ objects.js
@@ -0,0 +1,19 @@
+// const job = {
+//     title: 'Developer',
+//     location: 'New York',
+//     salary: 50000
+// }
+
+// console.log(new Date().toISOString()); 
+
+class Job {
+    constructor(jobTitle,place,salary) {
+        this.title = jobTitle;
+        this.location = place;
+        this.salary = salary;
+    }
+}
+
+const developer = new Job('Developer','New York',50000);
+const cook = new Job('Cook','Munich',35000)
+console.log(developer);
