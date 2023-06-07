bd683aaf0f7092bb6b2b3d5f1df7ed4b4f57fa65
beataj <beata@jasinski.com>
2023-06-07 12:26:52 -0700

    promises

--- async.js
+++ async.js
@@ -1,14 +1,18 @@
-const fs = require("fs");
+const fs = require("fs/promises");
 
 function readFile() {
     let fileData;
  
-    fs.readFile("data.txt", (error, fileData)=> {
-        console.log('File parsing done!');
-        console.log(fileData.toString());
+    // fs.readFile("data.txt", (error, fileData)=> {
+    //     console.log('File parsing done!');
+    //     console.log(fileData.toString());
+    // });
+  
+    fs.readFile('data.txt')
+    .then((fileData)=> {
+      console.log("File parsing done!");
+      console.log(fileData.toString());
     });
-  
-  
 
   console.log("Hi there!");
 }
